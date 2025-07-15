4162163e56caa4ec7940934022c4fe2b5aca825e
ronaldCueva <ronald.cj@outlook.com>
2025-07-15 16:51:08 -0500

    fix(database name): connection for users

--- scripts/init-users.sql
+++ scripts/init-users.sql
@@ -1,10 +1,10 @@
 -- Usuario de solo lectura (Power BI)
 CREATE USER IF NOT EXISTS 'read_only_user'@'%' IDENTIFIED BY 'REPLACE_WITH_PASSWORD1';
-GRANT SELECT ON plataforma.* TO 'read_only_user'@'%';
+GRANT SELECT ON SematecPlataform.* TO 'read_only_user'@'%';
 
 -- Usuario de lectura y escritura (ETL, aplicaciones)
 CREATE USER IF NOT EXISTS 'read_write_user'@'%' IDENTIFIED BY 'REPLACE_WITH_PASSWORD2';
-GRANT SELECT, INSERT, UPDATE, DELETE ON plataforma.* TO 'read_write_user'@'%';
+GRANT SELECT, INSERT, UPDATE, DELETE ON SematecPlataform.* TO 'read_write_user'@'%';
 
 -- Aplicar los cambios
 FLUSH PRIVILEGES;

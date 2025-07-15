20fbab5d0955915dfd2d80dfb323023307830bd2
ronaldCueva <ronald.cj@outlook.com>
2025-07-14 20:48:20 -0500

    update(password for users)

--- scripts/init-users.sql
+++ scripts/init-users.sql
@@ -1,10 +1,10 @@
 -- Usuario de solo lectura (Power BI)
-CREATE USER IF NOT EXISTS 'read_only_user'@'%' IDENTIFIED BY 'readonly_password';
+CREATE USER IF NOT EXISTS 'read_only_user'@'%' IDENTIFIED BY 'REPLACE_WITH_PASSWORD1';
 GRANT SELECT ON plataforma.* TO 'readonly_user'@'%';
 
 -- Usuario de lectura y escritura (ETL, aplicaciones)
-CREATE USER IF NOT EXISTS 'read_write_user'@'%' IDENTIFIED BY 'readwrite_password';
-GRANT SELECT, INSERT, UPDATE, DELETE ON plataforma.* TO 'readwrite_user'@'%';
+CREATE USER IF NOT EXISTS 'read_write_user'@'%' IDENTIFIED BY 'REPLACE_WITH_PASSWORD2';
+GRANT SELECT, INSERT, UPDATE, DELETE ON plataforma.* TO 'read_write_user'@'%';
 
 -- Aplicar los cambios
 FLUSH PRIVILEGES;
